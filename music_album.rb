5ec80632f0487c50be39e7009e8a3f9436142033
Sumair <sumairqaisar992@gmail.com>
2022-05-04 02:57:54 +0500

    Implement MusicAlbum class properties

--- music_album.rb
+++ music_album.rb
@@ -1,5 +1,9 @@
-class MusicAlbum
-  def dosomething
-    puts 'something'
+require_relative 'item'
+
+class MusicAlbum < Item
+  def initialize(name, publish_date, on_spotify)
+    super(id, name, publish_date)
+    @name = name
+    @on_spotify = on_spotify
   end
 end
