6f805683d05f6f2548b44adba0533f41c54796dd
Sumair <sumairqaisar992@gmail.com>
2022-05-05 03:20:49 +0500

    Add shema file with item, label, genre and author tables

---
+++ schema/schema.sql
@@ -0,0 +1,33 @@
+--Label
+CREATE TABLE label(
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(100),
+  color VARCHAR(100)
+);
+
+-- genre
+CREATE TABLE genre(
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100)
+);
+
+--Author
+CREATE TABLE author(
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(100),
+  last_name VARCHAR(100)
+);
+
+--Item
+CREATE TABLE item (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    publish_date DATE,
+    archived BOOLEAN,
+    genre_id INT,
+    author_id INT,
+    label_id INT,
+    FOREIGN KEY (genre_id) REFERENCES genre(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (author_id) REFERENCES author(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (label_id) REFERENCES label(id) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
